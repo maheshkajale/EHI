e72686ee476e9f9e61704f08a94b1a65e353545c
Mahesh Kajale <mahesh.kajale@infobeans.com>
2021-04-26 09:50:46 +0530

    router module rm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
@@ -24,7 +23,6 @@ import { ContactsService } from './contacts/contacts.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatFormFieldModule,
